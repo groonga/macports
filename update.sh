865f84c53e2ae0fa8ac71352ca8b0263120814b1
Kouhei Sutou <kou@clear-code.com>
2011-02-09 10:45:33 +0900

    create MacPorts for groonga.

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+        echo "Usage: $0 VERSIOIN"
+        echo " e.g.: $0 1.1.0"
+        exit 1
+fi
+
+cd $(dirname $0)
+
+version=$1
+tar_gz=groonga-$version.tar.gz
+portfile=databases/groonga/Portfile
+
+sed -E -i'' -e "s/^(version +)[a-z0-9.\-]+/\1$version/" $portfile
+
+curl -o $tar_gz -L http://groonga.org/files/groonga/$tar_gz
+
+for type in md5 sha1 rmd160; do
+        hash=$(openssl dgst -$type $tar_gz | cut -f 2 -d ' ')
+        sed -E -i'' -e "s/($type +)[a-z0-9]+/\1$hash/" $portfile
+done
+
+portindex
+
+rm $tar_gz
